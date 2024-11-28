46d34c8ea23acec03afe299193618af78b296c16
Tom van Ooijen <t.vanooijen@student.tudelft.nl>
2024-11-28 16:46:43 +0100

    Update precon.cpp

--- rbvms/precon.cpp
+++ rbvms/precon.cpp
@@ -24,9 +24,9 @@ void JacobianPreconditioner::SetOperator(const Operator &op)
       HypreParMatrix* Jpp = dynamic_cast<HypreParMatrix*>(&jacobian->GetBlock(1,1));
       HypreParMatrix *Jpp2 = const_cast<HypreParMatrix*>(Jpp);
     //  prec[1] = new HypreSmoother();//*Jpp);
-      *amg = new HypreBoomerAMG(Jpp);
+      amg = new HypreBoomerAMG(Jpp);
 
-      amg->SetPrintLevel(0);
+      amg.SetPrintLevel(0);
       //8 i1-Jacobi smoother. Try 6 (symmetric gauss-seidel) next, afterwards 16 (ILU)
       // amg->SetRelaxType(16);
       // //Aggressive coarsening
