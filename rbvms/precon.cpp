0e6646d24e42b532538032aaaf662791a52a6762
Tom van Ooijen <t.vanooijen@student.tudelft.nl>
2024-12-05 17:29:31 +0100

    Hopefully something happens

--- rbvms/precon.cpp
+++ rbvms/precon.cpp
@@ -14,7 +14,7 @@ using namespace RBVMS;
 void JacobianPreconditioner::SetOperator(const Operator &op)
 {
    BlockOperator *jacobian = (BlockOperator *) &op;
-
+   bool Resetter(true);
    if (prec[0] == nullptr)
    {
       prec[0] = new HypreILU();//*Jpp);new HypreSmoother();//HypreILU()
@@ -38,7 +38,9 @@ void JacobianPreconditioner::SetOperator(const Operator &op)
    for (int i = 0; i < prec.Size(); ++i)
    {
       //std::cout << "\nSetting preconditioner as operator" << std::endl;
-      prec[i]->SetOperator(jacobian->GetBlock(i,i));
+      
+      if (Resetter)
+         prec[i]->SetOperator(jacobian->GetBlock(i,i));
       SetDiagonalBlock(i, prec[i]);
 
       for (int j = i+1; j < prec.Size(); ++j)
@@ -46,6 +48,7 @@ void JacobianPreconditioner::SetOperator(const Operator &op)
          SetBlock(j,i, const_cast<Operator*>(&jacobian->GetBlock(j,i)));
       }
    }
+   Resetter(false);
 }
 
 // Destructor
