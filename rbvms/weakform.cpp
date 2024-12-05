d9a6d68f5587374ad5ae8f5e54f027a6b5cbc1af
Ivar Kuipers <ivarkuipers@live.nl>
2024-12-05 08:51:58 +0100

    Update weakform.cpp

--- rbvms/weakform.cpp
+++ rbvms/weakform.cpp
@@ -472,11 +472,11 @@ void IncNavStoIntegrator::AssembleElementGrad(
 
       auto TimeStart4 = std::chrono::high_resolution_clock::now();
       // Momentum - Pressure block (w,p)
-      for (int i_p = 0; i_p < dof_p; ++i_p)
+      for (int j_u = 0; j_u < dof_u; ++j_u)
       {
          for (int dim_u = 0; dim_u < dim; ++dim_u)
          {
-            for (int j_u = 0; j_u < dof_u; ++j_u)
+            for (int i_p = 0; i_p < dof_p; ++i_p)
             {
                (elmats01)(j_u + dof_u * dim_u, i_p)
                += (shg_p(i_p,dim_u)*tau_m*ushg_u(j_u)
