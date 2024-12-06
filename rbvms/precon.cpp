cf133fe01e5995db807090678509739128fb6757
Tom van Ooijen <t.vanooijen@student.tudelft.nl>
2024-12-06 13:11:21 +0100

    Testing parasails again because performance has somehow changed

--- rbvms/precon.cpp
+++ rbvms/precon.cpp
@@ -21,15 +21,15 @@ void JacobianPreconditioner::SetOperator(const Operator &op)
       std::cout << "Making a new precondtioner\n";
    }
    
-   //prec[0]->SetOperator(jacobian->GetBlock(0,0));
+   prec[0]->SetOperator(jacobian->GetBlock(0,0));
    if (prec[1] == nullptr)
    {
       HypreParMatrix* Jpp = dynamic_cast<HypreParMatrix*>(&jacobian->GetBlock(1,1));
       HypreParMatrix *Jpp2 = const_cast<HypreParMatrix*>(Jpp);
     //  prec[1] = new HypreSmoother();//*Jpp);
-      HypreSmoother *Parasails = new HypreSmoother(*Jpp);
+      HypreSmoother *Parasails = new HypreParaSails(*Jpp);
       //Parasails->SetParams(0.05, 1);
-      //Parasails->SetFilter(0.01); 
+      Parasails->SetFilter(0.01); 
       //Parasails->SetReuse(1);        
       //Parasails->SetLogging(1); 
       prec[1] = Parasails;//*Jpp);
@@ -40,7 +40,7 @@ void JacobianPreconditioner::SetOperator(const Operator &op)
    for (int i = 0; i < prec.Size(); ++i)
    {
       //std::cout << "\nSetting preconditioner as operator" << std::endl;
-      prec[i]->SetOperator(jacobian->GetBlock(i,i));
+      //prec[i]->SetOperator(jacobian->GetBlock(i,i));
       SetDiagonalBlock(i, prec[i]);
 
       for (int j = i+1; j < prec.Size(); ++j)
