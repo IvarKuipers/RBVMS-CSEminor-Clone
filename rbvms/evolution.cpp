3d5912598707e5dbbc3d40a05b761e35c3a42181
Tom van Ooijen <t.vanooijen@student.tudelft.nl>
2025-01-09 10:21:22 +0100

    Update evolution.cpp

--- rbvms/evolution.cpp
+++ rbvms/evolution.cpp
@@ -389,7 +389,7 @@ BlockOperator & ParTimeDepBlockNonlinForm::GetGradient(const Vector &x) const
    //Has to be true to work with the reset
    auto Jacobi_end = std::chrono::high_resolution_clock::now();
    auto Jacobi_duration = std::chrono::duration_cast<std::chrono::microseconds>(Jacobi_end - Jacobi_start).count();
-   std::cout << std::endl <<"Setup time for new preconditioner:  " << Precon_duration/1000000.0 <<  std::endl;
+   std::cout << std::endl <<"Setup time for new preconditioner:  " << Jacobi_duration/1000000.0 <<  std::endl;
    hasGrad = false;
    return *pBlockGrad;
 }
